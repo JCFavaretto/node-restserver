f230e416354c7fc5c10152a7f229d5ae6d661fcc
JCFavaretto <jeycepoint@gmail.com>
2021-01-04 02:04:21 -0300

    heroku uri

--- server/config/config.js
+++ server/config/config.js
@@ -12,8 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://jaycepoint:3GIKahGrjfvDKsM3@cluster0.v3ihe.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
